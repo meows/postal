a04bccef5d4c6f95b64e67fd1d864760ad18ccbd
meow <meow@>
2023-10-21 12:43:33 -0700

    fixed excludeLetter

--- src/lib/exclude_letter.ts
+++ src/lib/exclude_letter.ts
@@ -1,3 +1,10 @@
+/**
+
+Given an array of letters will return an uppercase alphabet string for making
+regexes that exclude those letters.
+
+*/
+
 export default function excludeLetter(letters: string[]): string {
    const mask = new RegExp(`[${letters.join("")}]`, "g")
    return "ABCDEFGHIJKLMNOPQRSTUVWXYZ".replace(mask, "")
