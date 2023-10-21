c69ca31cbb52ee6822ce86824acc860ea0ec9492
meow <meow@>
2023-10-21 12:57:17 -0700

    update

--- src/lib/code_to_regex.ts
+++ src/lib/code_to_regex.ts
@@ -10,19 +10,19 @@
 export default function codeToRegex(code: string): RegExp {
    return new RegExp(
       code
-         .split('')
+         .split("")
          .map((char) => {
             switch (char) {
-               case '@':
-                  return '[a-zA-Z]';
-               case '#':
-                  return '[0-9]';
-               case '*':
-                  return '.';
+               case "@":
+                  return "[a-zA-Z]"
+               case "#":
+                  return "[0-9]"
+               case "*":
+                  return "."
                default:
-                  return char;
+                  return char
             }
          })
-         .join('')
-   );
+         .join("")
+   )
 }
